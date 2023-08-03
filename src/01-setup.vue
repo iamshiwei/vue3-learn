3a7dba43be34e4d3db25e29dc5b97fd90a68afac
iamshiwei <2587159769@qq.com>
2023-08-03 22:26:51 +0800

    init

---
+++ src/01-setup.vue
@@ -0,0 +1,28 @@
+<!-- <script>
+  export default {
+    setup(){
+      console.log('setup');
+      const message = 'this is message'
+      const loadMsg = () => {
+        console.log(message);
+      }
+      return {
+        message,
+        loadMsg
+      }
+    },
+    beforeCreate() {
+      console.log('boforecreate');
+    },
+  }
+</script> -->
+<script setup>
+  const message = 'this is message'
+      const loadMsg = () => {
+        console.log(message);
+      }
+</script>
+<template>
+  <div>{{ message }}</div>
+  <button @click="loadMsg">打印</button>
+</template>
---
+++ src/02-ref.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const state = reactive({
+  count: 0
+})
+const setCount = () => {
+  count.value++
+}
+const count = ref(0)
+</script>
+<template>
+  <div>
+    <button @click="setCount">{{ state.count }}</button>
+    <button @click="setCount">{{ count }}</button>
+  </div>
+</template>
